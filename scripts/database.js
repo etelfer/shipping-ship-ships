26046b688fd3b33247d199592ad18a45966129e1
Erin Telfer <eringtelfer@gmail.com>
2025-08-26 20:21:06 -0500

    added cargoships data and function to create html

--- scripts/database.js
+++ scripts/database.js
@@ -31,7 +31,29 @@ const database = {
         name: "Gulf Cargo",
         dockId: 3
     }
-];
+],
+    cargoShips: [
+    {
+        id: 1,
+        name: "Esso Atlantic",
+        haulerId: 3
+    },
+    {
+        id: 2,
+        name: "Triton",
+        haulerId: 2
+    },
+    {
+        id: 3,
+        name: "Neptune",
+        haulerId: 1
+    },
+    {
+        id: 4,
+        name: "Abyssal",
+        haulerId: 4
+    }
+]
 }
 
 export const getDocks = () => {
@@ -40,4 +62,8 @@ export const getDocks = () => {
 
 export const getHaulers = () => {
     return structuredClone(database.haulers);
+}
+
+export const getCargoShips = () => {
+    return structuredClone(database.cargoShips);
 }
